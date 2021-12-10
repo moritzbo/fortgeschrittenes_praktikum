8a1051d009dd40ed15a68e65fc18bd7e107af4e2
Moritz Bosse <moritz.bosse@tu-dortmund.de>
2021-12-10 17:05:25 +0100

    change

--- V48Dipol/python/plot.py
+++ V48Dipol/python/plot.py
@@ -165,17 +165,7 @@ plt.plot(T2p, i2p-(params2[0]*np.exp(params2[1]*T2p)), "xr", label="Messdaten :=
 plt.xlim(200,320)
 plt.ylim(-1,3)
 
-NFLStrom1 = dsfds - (params1[0]*np.exp(params1[1]*T1))
-NFLStrom2 = fsddf - (params2[0]*np.exp(params2[1]*T2))
 
-
-print(T1[27])
-print(T1[54])
-INT1 = 0
-for i in range(27):
-   INT1 = INT1 + (T1[i+28] - T1[i+27]) * (NFLStrom1[i+28] + NFLStrom1[i+27])/2
-   print(INT1) 
-print(f"HIER HIER HIER: {INT1:.2f}")
 
 plt.ylabel(r'I $[\si{\pico\ampere}]$')
 plt.xlabel(r'T $[\si{\kelvin}]$')
@@ -184,6 +174,99 @@ plt.grid()
 plt.legend()
 plt.savefig("build/untegrrund2.pdf")
 
+plt.clf()
+
+NFLStrom1 = dsfds - (params1[0]*np.exp(params1[1]*T1))
+NFLStrom2 = fsddf - (params2[0]*np.exp(params2[1]*T2))
+
+
+print(T1[27])
+print(T1[54])
+INT1 = 0
+
+arrayIntvalues1 = []
+
+array1 = [28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54]
+for j in array1:
+    for i in range(27 - array1.index(j)):
+        INT1 = INT1 + (T1[i+j] - T1[i+j-1]) * (NFLStrom1[i+j] + NFLStrom1[i+j-1])/2
+    arrayIntvalues1.append(INT1)
+    INT1 = 0
+print(f"HIER HIER HIER: {arrayIntvalues1}")
+
+T1NEU = []
+T2NEU = []
+NFLStrom1NEU = []
+NFLStrom2NEU = []
+
+for m in range(27):
+    T1NEU.append(T1[27+m])
+    NFLStrom1NEU.append(NFLStrom1[27+m])
+
+for n in range(18):
+    T2NEU.append(T2[15+n])
+    NFLStrom2NEU.append(NFLStrom2[27+m])
+
+for i in range(27):
+   INT1 = INT1 + (T1[i+28] - T1[i+27]) * (NFLStrom1[i+28] + NFLStrom1[i+27])/2
+   print(INT1) 
+print(f"HIER HIER HIER: {INT1:.2f}")
+
+
+print(T2[15])
+print(T2[33])
+INT2 = 0
+arrayIntvalues2 = []
+
+array2 = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]
+for j in array2:
+    for i in range(18 - array2.index(j)):
+        INT2 = INT2 + (T2[i+j] - T2[i+j-1]) * (NFLStrom2[i+j] + NFLStrom2[i+j-1])/2
+    arrayIntvalues2.append(INT2)
+    INT2 = 0
+print(f"HIER HIER HIER: {arrayIntvalues2}")
+
+INTWERT1 = np.log(np.array(arrayIntvalues1)/(np.array(NFLStrom1NEU) * 1))
+INTWERT2 = np.log(np.array(arrayIntvalues2)/(np.array(NFLStrom2NEU) * 2))
+
+def lol1(x, a, b):
+    return a*x+b
+
+params, covariance_matrix = curve_fit(lol1, 1/np.array(T1NEU), INTWERT1)
+
+uncertainties = np.sqrt(np.diag(covariance_matrix))
+print("YOYOOYOYOYOYOYOOYOYOYOYO:")
+for name, value, uncertainty in zip('ab', params, uncertainties): 
+    print(f'{name} = {value:.4f} ± {uncertainty:.4f}')
+
+x = np.linspace(0.00373,0.0042)
+
+plt.plot(1/np.array(T1NEU), INTWERT1, "kx")
+
+plt.plot(x, params[0]*x+params[1],
+        'k--',
+        
+        linewidth=1)
+
+#def lol2(x, a, b):
+#    return a*x+b
+#
+#params, covariance_matrix = curve_fit(lol2, 1/np.array(T2NEU), INTWERT2)
+#
+#uncertainties = np.sqrt(np.diag(covariance_matrix))
+#print("WASGEEEEEEEEEHT:")
+#for name, value, uncertainty in zip('ab', params, uncertainties): 
+#    print(f'{name} = {value:.4f} ± {uncertainty:.4f}')
+#
+#x = np.linspace(240,280)
+#
+#plt.plot(x, params[0]*x+params[1],
+#        'b--',
+#        
+#        linewidth=1)
+#plt.plot(T2NEU, INTWERT2, "bx")
+
+plt.savefig("build/lol1.pdf")
 plt.clf()
 
 ab = T1an 
